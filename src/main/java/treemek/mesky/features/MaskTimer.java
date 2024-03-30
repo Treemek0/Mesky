bc7f29fbaa860d815d8d3ab847108b1c0f354c54
Treemek <67553502+Treemek0@users.noreply.github.com>
2024-03-30 18:25:32 +0100

    Fixed bonzo mask
    I didnt test this before, because i didnt know what caused this problem. But its fixed now and it was pretty easy

--- src/main/java/treemek/mesky/features/MaskTimer.java
+++ src/main/java/treemek/mesky/features/MaskTimer.java
@@ -9,8 +9,10 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.IChatComponent;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -19,6 +21,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import treemek.mesky.Reference;
+import treemek.mesky.config.GuiLocation;
 import treemek.mesky.config.SettingsConfig;
 import treemek.mesky.handlers.RenderHandler;
 import treemek.mesky.utils.Alerts;
@@ -34,7 +37,7 @@ public class MaskTimer extends GuiScreen {
 	@SubscribeEvent(priority = EventPriority.HIGH)
 	public void onChat(ClientChatReceivedEvent event) {
 		
-        String message = event.message.getUnformattedText();
+        String message = net.minecraft.util.StringUtils.stripControlCodes(event.message.getUnformattedText());
         ItemStack mask = Minecraft.getMinecraft().thePlayer.getCurrentArmor(3);
         
         if(message.contains(":")) return; // only from server not players
@@ -42,20 +45,26 @@ public class MaskTimer extends GuiScreen {
 		if(message.contains("Bonzo's Mask") && message.contains("saved your life!") && SettingsConfig.BonzoTimer) {
 			if(mask != null) {
 				String bonzoLore = StringUtils.join(Utils.getItemLore(mask), " ");
-				
-					String[] bonzoLoreSplit = bonzoLore.split(" ");
-					for (int i = bonzoLoreSplit.length; i == 0; i--){
-						Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(bonzoLoreSplit[i]));
-					    if (bonzoLoreSplit[i].equalsIgnoreCase("Cooldown:")) {
-					       BonzoCooldownSeconds = Integer.parseInt(bonzoLoreSplit[i-1]);
-					       BonzoMaskActivated = true;
-					       Alerts.DisplayCustomAlerts(bonzoLoreSplit[i-1], 1000);
-					       break;
-					}
-					
-                }
+				String[] bonzoLoreSplit = bonzoLore.split(" ");
+				for (int i = 0; i < bonzoLoreSplit.length; i++){
+				    if (bonzoLoreSplit[i].contains("Cooldown:")) {
+				    	String seconds = net.minecraft.util.StringUtils.stripControlCodes(bonzoLoreSplit[i+1].substring(0, bonzoLoreSplit[i+1].length() - 1));
+				    	
+				    	mc.thePlayer.addChatMessage(new ChatComponentText(seconds));
+				    	try {
+				    		BonzoCooldownSeconds = Integer.parseInt(seconds);
+						} catch (Exception e) {
+							mc.thePlayer.addChatMessage(new ChatComponentText(e.toString()));
+						}
+				    	
+				    	BonzoMaskActivated = true;
+				    	Alerts.DisplayCustomAlerts("Bonzo Mask", 1000);
+				    	return;
+				    }
+				}
 			}
 		}
+		
 		
 		if(message.contains("Your Spirit Mask saved your life!") && SettingsConfig.SpiritTimer) {
 			if (mask != null) {
@@ -98,7 +107,7 @@ public class MaskTimer extends GuiScreen {
 		String cooldownTimer = Math.round(BonzoCooldownSeconds) + "s";
 		
 		// Calculate the position to render the timer
-        int x = resolution.getScaledWidth() / 10;
+		int x = resolution.getScaledWidth() / 2;
         int y = resolution.getScaledHeight() / 3 - 10;
 		
         RenderHandler.drawText(cooldownTimer, x + 17, y, 1, false, 0xFFFFFF);
@@ -114,7 +123,7 @@ public class MaskTimer extends GuiScreen {
 		String cooldownTimer = Math.round(SpiritCooldownSeconds) + "s";
 		
 		// Calculate the position to render the timer
-        int x = resolution.getScaledWidth() / 10;
+        int x = resolution.getScaledWidth() / 2;
         int y = resolution.getScaledHeight() / 3 - 20;
 		
         RenderHandler.drawText(cooldownTimer, x + 17, y, 1, false, 0xFFFFFF);
